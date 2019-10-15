a3b16911ecc26386207a1da909c60fc6bc058500
Pointerasad <asadoraclebd7@gmail.com>
2019-10-15 10:00:11 +0600

    added another file

---
+++ Project/src/BookStore/Pattern.java
@@ -0,0 +1,71 @@
+package BookStore;
+
+import java.util.Scanner;
+
+public class Pattern {
+	
+	public static void main(String[] args) 
+	{
+	Scanner sc = new Scanner(System.in);
+
+	//Taking rows value from the user
+
+	System.out.println("How many rows you want in this pattern?");
+
+	int rows = sc.nextInt();
+
+	System.out.println("Here is your pattern..!!!");
+
+	//Printing upper half of the pattern
+
+	for (int i = 1; i <= rows; i++) 
+	{
+	for (int j = 1; j <= i; j++) 
+	{
+
+	System.out.print(j+" "); 
+	}
+	// printing spaces 
+	for(int sp=1; sp <= 2 * (rows - i); sp++)
+	{
+	System.out.print(" "); 
+	}
+
+	for (int j = i; j >= 1; j--) 
+	{
+
+	System.out.print(j+" "); 
+	}
+
+
+	System.out.println(); 
+	}
+
+	//Printing lower half of the pattern
+
+	for (int i = rows-1; i >= 1; i--)
+	{
+	for (int j = 1; j <= i; j++)
+	{
+	System.out.print(j+" ");
+	}
+	// printing spaces
+	for(int sp=1; sp <= 2 * (rows - i); sp++)
+	{
+	System.out.print(" "); 
+	}
+
+	for (int j = i; j >= 1; j--) 
+	{
+
+	System.out.print(j+" "); 
+	}
+
+	System.out.println();
+	}
+
+	//Closing the resources
+
+	sc.close();
+	}
+	}
